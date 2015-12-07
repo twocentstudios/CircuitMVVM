5bf98fd85c2c48fd70b3fab5d5e26c1a5834b0a6
Christopher Trott <chris@twocentstudios.com>
2015-12-08 01:33:28 +0900

    Use empty Impulse protocol and give up on convenience filter/unwrap

--- Circuit.playground/Contents.swift
+++ Circuit.playground/Contents.swift
@@ -1,47 +1,41 @@
 
 import Foundation
 
-
-extension RangeReplaceableCollectionType where Generator.Element : Equatable {
-    mutating func removeObject(object : Generator.Element) {
-        if let index = self.indexOf(object) {
-            self.removeAtIndex(index)
-        }
-    }
-}
-
 class Circuit {  // Signal
-    
     private var etches = [Etch]()
     
-    private let eventQueue: dispatch_queue_t = dispatch_queue_create("circuit_event_queue", DISPATCH_QUEUE_SERIAL)
+    private let impulseQueue: dispatch_queue_t = dispatch_queue_create("circuit_impulse_queue", DISPATCH_QUEUE_SERIAL)
     private let defaultDispatchQueue: dispatch_queue_t = dispatch_queue_create("circuit_default_dispatch_queue", DISPATCH_QUEUE_CONCURRENT)
     
-    func addEtch(etch: Etch) {
-        dispatch_async(eventQueue) { _ in
+    /// Add an etch to this circuit from any thread.
+    internal func addEtch(etch: Etch) {
+        dispatch_async(impulseQueue) { _ in
             self.etches.append(etch)
         }
     }
     
-    func sendEvent(event: Event) {
-        dispatch_async(eventQueue) { _ in
-            self.recursiveSendToEtches(event, etches: self.etches)
+    /// Send an impulse from any thread.
+    internal func sendImpulse(impulse: Impulse) {
+        dispatch_async(impulseQueue) { _ in
+            self.recursiveSendImpulseToEtches(impulse, etches: self.etches)
         }
     }
     
-    private func sendEvents(events: [Event]) {
-        for event in events {
-            self.sendEvent(event)
+    private func sendImpulses(impulses: [Impulse]) {
+        for impulse in impulses {
+            self.sendImpulse(impulse)
         }
     }
     
     private func removeDeadEtch(deadEtch: Etch) {
-        dispatch_async(eventQueue) { _ in
-            self.etches.removeObject(deadEtch)
+        dispatch_async(impulseQueue) { _ in
+            if let index = self.etches.indexOf(deadEtch) {
+                self.etches.removeAtIndex(index)
+            }
         }
     }
     
-    private func recursiveSendToEtches(event: Event, etches: [Etch]) {
+    private func recursiveSendImpulseToEtches(impulse: Impulse, etches: [Etch]) {
         if (etches.count == 0) {
             return
         }
@@ -51,61 +45,58 @@ class Circuit {  // Signal
         
         if (!currentEtch.alive()) {
             self.removeDeadEtch(currentEtch)
-            recursiveSendToEtches(event, etches: remainingEtches)
-            return
-        }
-        
-        if (false) { // etch.filter == event
-            recursiveSendToEtches(event, etches: remainingEtches)
+            recursiveSendImpulseToEtches(impulse, etches: remainingEtches)
             return
         }
         
         if let filter = currentEtch.filter {
-            if filter != event {
-                recursiveSendToEtches(event, etches: remainingEtches)
+            if !filter(impulse) {
+                recursiveSendImpulseToEtches(impulse, etches: remainingEtches)
                 return
             }
         }
         
         let queue = currentEtch.queue ?? self.defaultDispatchQueue
         dispatch_async(queue) { _ in
-            if let events = currentEtch.dispatch(event) {
-                self.sendEvents(events)
+            if let impulses = currentEtch.dispatch(impulse) {
+                self.sendImpulses(impulses)
             }
-            self.recursiveSendToEtches(event, etches: remainingEtches)
+            self.recursiveSendImpulseToEtches(impulse, etches: remainingEtches)
             return
         }
     }
 }
 
-struct Etch {  // Observer
+struct Etch { // "Observer"
+    /// A unique identifier for this Etch. For supporting equatable.
     private let id = NSUUID()
     
-    // aliveBlock (Void -> Bool)?
+    /// When the etch should permanently cease to receieve impulses.
     private(set) internal var alive: (() -> Bool) = { true }
     
-    // filter block (Type where kind of Impulse)
-    private(set) internal var filter: Event? = nil
+    /// Which impuluses this etch should be dispatched for.
+    private(set) internal var filter: (Impulse -> Bool)? = nil
     
-    // preferred scheduler to on which to run dispatch if necessary
+    /// Preferred scheduler to on which to run dispatch if necessary.
+    /// If not provided, the circuit will run it on a default queue.
     private(set) internal var queue: dispatch_queue_t? = nil
     
-    // code to run
-    private(set) internal var dispatch: (Event -> [Event]?) = { _ in nil }
+    /// The code to run in response to a matching Impulse.
+    private(set) internal var dispatch: (Impulse -> [Impulse]?) = { _ in nil }
     
-    func withAlive(block: (() -> Bool)) -> Etch {
+    internal func withAlive(block: (() -> Bool)) -> Etch {
         var etch = self
         etch.alive = block
         return etch
     }
     
-    func withFilter(filter: Event?) -> Etch {
+    internal func withFilter(block: (Impulse -> Bool)?) -> Etch {
         var etch = self
-        etch.filter = filter
+        etch.filter = block
         return etch
     }
     
-    func withDispatch(dispatch: (Event -> [Event]?)) -> Etch {
+    internal func withDispatch(dispatch: (Impulse -> [Impulse]?)) -> Etch {
         var etch = self
         etch.dispatch = dispatch
         return etch
@@ -117,23 +108,15 @@ func ==(lhs: Etch, rhs: Etch) -> Bool {
     return lhs.id == rhs.id
 }
 
-enum Event {
-    case RequestReadTodos // (Sentinel)
-    case ResponseTodos // ([String])
-}
+/// A message. Intended to be an enum type.
+protocol Impulse { } // "Event"
 
-protocol Impulse {
-    typealias Value
-    
-    var value: Value { get }
-}
 
-struct RequestReadTodos: Impulse {
-    let value: Sentinel
-}
+/// EXAMPLES
 
-struct ResponseTodos: Impulse {
-    let value: [Int]
+enum ModelImpulse: Impulse {
+    case RequestReadTodos(Sentinel)
+    case ResponseTodos([String])
 }
 
 struct Sentinel { }
