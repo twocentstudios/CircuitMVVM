75fe093830433eaa623267229cb7360e39489672
Christopher Trott <chris@twocentstudios.com>
2015-12-10 16:34:18 +0900

    Update pod

--- Example/CircuitMVVM/AppDelegate.swift
+++ Example/CircuitMVVM/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+import CircuitMVVM
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -15,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
 
